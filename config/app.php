cd04fe73e1c58871c5eb5448994e708d988b2fb1
ander96 <anderson9daza6@gmail.com>
2021-11-16 20:42:48 -0500

    config protocol

--- config/app.php
+++ config/app.php
@@ -51,8 +51,8 @@ return [
     | your application so that it is used when running Artisan tasks.
     |
     */
-    'url' => env('APP_URL', 'https://lava-coches-aguazul.herokuapp.com'),
-    // 'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'https://lava-coches-aguazul.herokuapp.com'),
+    'url' => env('APP_URL', 'https://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
