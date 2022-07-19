6a3b2d2c17966d1db869b7ff6dfc8b319c610274
anderDaZu <anderda-96@hotmail.com>
2022-07-18 20:58:47 -0500

    change in date

--- app/Http/Controllers/Api/v1/user/AppointmentMorningController.php
+++ app/Http/Controllers/Api/v1/user/AppointmentMorningController.php
@@ -206,7 +206,7 @@ class AppointmentMorningController extends Controller
     public function checkEmployees(Vehicle $vehicle, Service $service, Space $space)
     {
         // $today = date('Y-m-d');
-        $today = date('Y-m-d', strtotime('2022-07-15'));
+        $today = date('Y-m-d', strtotime('2022-07-16'));
         // $hour_now = date('H:i:s');
         $hour_now = date('H:i:s', strtotime('08:00:00'));
 
@@ -316,7 +316,7 @@ class AppointmentMorningController extends Controller
         ]);
 
         // $today = date('Y-m-d');
-        $today = date('Y-m-d', strtotime('2022-07-15'));
+        $today = date('Y-m-d', strtotime('2022-07-16'));
         // $hour_now = date('H:i:s');
         $hour_now = date('H:i:s', strtotime('08:00:00'));
         $available = date('H:i:s', strtotime('+10 minute', strtotime($hour_now)));
