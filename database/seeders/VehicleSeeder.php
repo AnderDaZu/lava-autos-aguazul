b81a8e133aa8bc875e63111319608d54cef0f7a7
anderDaZu <anderda-96@hotmail.com>
2022-07-24 14:36:34 -0500

    quit vehicles seeder

--- database/seeders/VehicleSeeder.php
+++ database/seeders/VehicleSeeder.php
@@ -18,40 +18,40 @@ class VehicleSeeder extends Seeder
     public function run()
     {
 
-        Vehicle::create([
-            'plate' => 'abc123',
-            'color_id' => 1,
-            'modelcar_id' => 1,
-            'client_id' => 10
-        ]);
+        // Vehicle::create([
+        //     'plate' => 'abc123',
+        //     'color_id' => 1,
+        //     'modelcar_id' => 1,
+        //     'client_id' => 10
+        // ]);
 
-        Vehicle::create([
-            'plate' => 'acb132',
-            'color_id' => 4,
-            'modelcar_id' => 3,
-            'client_id' => 10
-        ]);
+        // Vehicle::create([
+        //     'plate' => 'acb132',
+        //     'color_id' => 4,
+        //     'modelcar_id' => 3,
+        //     'client_id' => 10
+        // ]);
 
-        Vehicle::create([
-            'plate' => 'cba321',
-            'color_id' => 3,
-            'modelcar_id' => 2,
-            'client_id' => 11
-        ]);
+        // Vehicle::create([
+        //     'plate' => 'cba321',
+        //     'color_id' => 3,
+        //     'modelcar_id' => 2,
+        //     'client_id' => 11
+        // ]);
 
-        Vehicle::create([
-            'plate' => 'cab312',
-            'color_id' => 6,
-            'modelcar_id' => 4,
-            'client_id' => 11
-        ]); 
+        // Vehicle::create([
+        //     'plate' => 'cab312',
+        //     'color_id' => 6,
+        //     'modelcar_id' => 4,
+        //     'client_id' => 11
+        // ]); 
 
-        Vehicle::create([
-            'plate' => 'abc122',
-            'color_id' => 5,
-            'modelcar_id' => 3,
-            'client_id' => 12
-        ]);
+        // Vehicle::create([
+        //     'plate' => 'abc122',
+        //     'color_id' => 5,
+        //     'modelcar_id' => 3,
+        //     'client_id' => 12
+        // ]);
 
     }
 }
