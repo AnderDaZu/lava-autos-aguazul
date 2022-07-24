9e2b4861c3ad35ad0aaa16663b80f4746c2106f5
anderDaZu <anderda-96@hotmail.com>
2022-07-24 14:44:21 -0500

    quit appointments seeder

--- database/seeders/AppointmentSeeder.php
+++ database/seeders/AppointmentSeeder.php
@@ -14,17 +14,17 @@ class AppointmentSeeder extends Seeder
      */
     public function run()
     {
-        Appointment::create([
-            'date' => '2022-04-15',
-            'hour_start' => '07:45',
-            'hour_end' => '10:00',
-            'horario_id' => 1,
-            'service_id' => 6,
-            'vehicle_id' => 5,
-            'employee_id' => 8,
-            'client_id' => 10,
-            'state_id' => 2,
-        ]); 
+        // Appointment::create([
+        //     'date' => '2022-04-15',
+        //     'hour_start' => '07:45',
+        //     'hour_end' => '10:00',
+        //     'horario_id' => 1,
+        //     'service_id' => 6,
+        //     'vehicle_id' => 5,
+        //     'employee_id' => 8,
+        //     'client_id' => 10,
+        //     'state_id' => 2,
+        // ]); 
 
     }
 }
